39512215306e1e2441f4260efa666950ef0cb454
System Administrator <root@Isaacs-MacBook-Pro.local>
2024-09-27 17:07:57 +0300

    First Commit

--- src/index.js
+++ src/index.js
@@ -2,13 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { AppProvider } from '@shopify/polaris';
+import enTranslations from '@shopify/polaris/locales/en.json';
+import '@shopify/polaris/build/esm/styles.css';  // Import Polaris styles
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <AppProvider i18n={enTranslations}>
+
     <App />
-  </React.StrictMode>
+  </AppProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
